4c067c76d4cf8a903dcbf748d99c3817ae37febc
ArrowsGD <58781705+ArrowsGD@users.noreply.github.com>
2021-06-04 00:41:07 -0400

    Create DataManager.java

---
+++ DataManager.java
@@ -0,0 +1,68 @@
+package me.arrows.HotKey;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+
+public class DataManager {
+	
+	private Main plugin;
+	private FileConfiguration dataConfig = null;
+	private File configFile = null;
+	
+	public DataManager(Main plugin) {
+		this.plugin = plugin;
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		
+	}
+	
+	public void reloadConfig() {
+		if(this.configFile == null)
+			this.configFile = new File(this.plugin.getDataFolder(),"data.yml");
+		
+		this.dataConfig = YamlConfiguration.loadConfiguration(this.configFile);
+		
+		InputStream defaultStream = this.plugin.getResource("data.ylm");
+		if(defaultStream != null) {
+			YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defaultStream));
+			this.dataConfig.setDefaults(defaultConfig);
+		}
+		
+	}
+	
+	public FileConfiguration getConfig() {
+		if(this.dataConfig == null)
+			reloadConfig();
+		
+		return this.dataConfig;
+	}
+	
+	
+	public void saveConfig() {
+		if(this.dataConfig == null || this.configFile == null)
+			return;
+		
+		try {
+			this.getConfig().save(this.configFile);
+		} catch (IOException e) {
+			plugin.getLogger().log(Level.SEVERE, "Could not save config to" + this.configFile, e);
+		}
+	}
+	
+	public void saveDefaultConfig() {
+		if(this.configFile == null)
+			this.configFile = new File(this.plugin.getDataFolder(),"data.ylm");
+			
+		if(!this.configFile.exists()) {
+			this.plugin.saveResource("data.ylm", false);
+		}
+	}
+
+}

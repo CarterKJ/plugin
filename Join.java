32413191452272392006656735e500a3473a010d
ArrowsGD <58781705+ArrowsGD@users.noreply.github.com>
2021-06-04 00:40:32 -0400

    Create Join.java

---
+++ Join.java
@@ -0,0 +1,18 @@
+package me.arrows.HotKey;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class Join implements Listener {
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		if(NPC.getNPCs() == null)
+			return;
+		if(NPC.getNPCs().isEmpty())
+			return;
+		NPC.addJoinacket(event.getPlayer());
+	}
+
+	}
